734f1a1a4872bd6595f0ae0563a8f8aae9e82d16
troydalldorf <troydalldorf@964d8396-bc2a-11dd-b9c8-e7e19bf6fede>
2008-12-07 18:11:17 +0000

    Change namespaces. Added some unit tests - more to come.

---
+++ trunk/RpnParser.UnitTests/IntegerTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Dalldorf.Expressions;
+
+namespace Dalldorf.Expressions.UnitTests
+{
+    [TestFixture]
+    public class IntegerTests
+    {
+        [Test]
+        public void IntegerAdd()
+        {
+            RpnParser rpn = new RpnParser();
+            Assert.AreEqual(2, rpn.Evaluate("1+1"));
+            Assert.AreEqual(3000, rpn.Evaluate("1000+2000"));
+            Assert.AreEqual(21, rpn.Evaluate("1+2+3+4+5+6"));
+        }
+
+        [Test]
+        public void IntegerAddNegative()
+        {
+            RpnParser rpn = new RpnParser();
+            Assert.AreEqual(1, rpn.Evaluate("-1+2"));
+            Assert.AreEqual(-3, rpn.Evaluate("-1+-2"));
+        }
+
+        [Test]
+        public void IntegerSub()
+        {
+            RpnParser rpn = new RpnParser();
+            Assert.AreEqual(0, rpn.Evaluate("1-1"));
+            Assert.AreEqual(1000, rpn.Evaluate("2000-1000"));
+        }
+
+        [Test]
+        public void IntegerSubNegative()
+        {
+            RpnParser rpn = new RpnParser();
+            Assert.AreEqual(-21, rpn.Evaluate("-6-5-4-3-2-1"));
+            Assert.AreEqual(-3, rpn.Evaluate("-1-2"));
+        }
+
+        [Test]
+        public void IntegerMultiply()
+        {
+            RpnParser rpn = new RpnParser();
+            Assert.AreEqual(6, rpn.Evaluate("2*3"));
+            Assert.AreEqual(3000, rpn.Evaluate("50*60"));
+        }
+
+        [Test]
+        public void IntegerDivide()
+        {
+            RpnParser rpn = new RpnParser();
+            Assert.AreEqual(3, rpn.Evaluate("6/2"));
+            Assert.AreEqual(2, rpn.Evaluate("5/2"));
+        }
+
+        [Test]
+        public void IntegerEqual()
+        {
+            RpnParser rpn = new RpnParser();
+            Assert.AreEqual(true, rpn.Evaluate("4=4"));
+            Assert.AreEqual(false, rpn.Evaluate("5=4"));
+            Assert.AreEqual(true, rpn.Evaluate("4==4"));
+            Assert.AreEqual(false, rpn.Evaluate("5==4"));
+            Assert.AreEqual(false, rpn.Evaluate("3+2==2+3"));
+        }
+
+        [Test]
+        public void IntegerGreaterThan()
+        {
+            RpnParser rpn = new RpnParser();
+            Assert.AreEqual(true, rpn.Evaluate("4>2"));
+            Assert.AreEqual(false, rpn.Evaluate("100>200"));
+            Assert.AreEqual(true, rpn.Evaluate("100>=100"));
+            Assert.AreEqual(false, rpn.Evaluate("100>=99"));
+            Assert.AreEqual(true, rpn.Evaluate("10+10>10-5"));
+        }
+
+        [Test]
+        public void IntegerLessThan()
+        {
+            RpnParser rpn = new RpnParser();
+            Assert.AreEqual(false, rpn.Evaluate("4<2"));
+            Assert.AreEqual(true, rpn.Evaluate("100<200"));
+            Assert.AreEqual(false, rpn.Evaluate("100<=100"));
+            Assert.AreEqual(true, rpn.Evaluate("100<=99"));
+            Assert.AreEqual(false, rpn.Evaluate("10+10<10-5"));
+        }
+    }
+}
--- trunk/RpnParser.UnitTests/Properties/AssemblyInfo.cs
+++ trunk/RpnParser.UnitTests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RpnParser")]
+[assembly: AssemblyTitle("RpnParser.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dalldorf Technologies, Inc.")]
-[assembly: AssemblyProduct("RpnParser")]
-[assembly: AssemblyCopyright("Copyright © Dalldorf 2007")]
+[assembly: AssemblyProduct("RpnParser.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Dalldorf Technologies 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fe9a9ceb-541b-483e-8e51-865e70f7741b")]
+[assembly: Guid("cbc34029-3955-4b28-8c7e-b3dd524b6f09")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
